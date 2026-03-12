05c8b85bee9235a676edb5e8a4a35ffaf11f87ed
Sinister-ss <eric85346666@gmail.com>
2026-03-12 15:03:00 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+BOT_TOKEN = "AAH1EMm8iMsG3s8UkRAx8JM4SJgnK7JijDA"
+GROUP_LINK = "https://t.me/MANCIN_HOLIC"
+CHANNEL_LINK = "https://t.me/MANCINGHOLICC"
+
+# Game settings
+FISHING_COOLDOWN = 30        # seconds between fishing
+DAILY_COOLDOWN = 86400       # 24 hours
+BOOST_DURATION = 3600        # 1 hour boost duration
